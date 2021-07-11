f7df8613efdeba789dfdcc0a392c7dd387df5987
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-11 18:59:39 +0300

    Adding forms file and views function for registration

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import Registration, UpdateUser, UpdateProfile, CommentForm, postImageForm
 
 # Create your views here.
+def register(request):
+  if request.method == 'POST':
+    form = Registration(request.POST)
+    if form.is_valid():
+      form.save()
+      email = form.cleaned_data['email']
+      username = form.cleaned_data.get('username')
+
+      messages.success(
+          request, f'Account for {username} created,you can now login')
+      return redirect('login')
+  else:
+    form = Registration()
+  return render(request, 'auth/registration.html', {"form": form})
+
