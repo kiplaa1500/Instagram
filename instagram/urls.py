bc870a5c47063ce242195456275a2dcfad6815f8
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-11 22:19:07 +0300

    Adding more urls route

--- instagram/urls.py
+++ instagram/urls.py
@@ -10,6 +10,17 @@ urlpatterns = [
   path('login/',auth_views.LoginView.as_view(template_name = 'auth/login.html'),name='login'),
   path('logout/',auth_views.LogoutView.as_view(template_name = 'auth/logout.html'),name='logout'),
   path('index/',main_views.index,name='index'),
+  path('update/',main_views.update_profile,name='update_profile'),
+  re_path(r'^comment/(?P<image_id>\d+)$',main_views.commenting,name='commenting'),
+  re_path(r'^likes/(?P<image_id>\d+)$',main_views.likes,name='likes'),
+  re_path(r'^allcomments/(?P<image_id>\d+)$',main_views.allcomments,name='allcomments'),
+  re_path(r'^search/$',main_views.search,name='search'),
+  path('post/',main_views.post,name='post'),
+  re_path(r'^post_profile/(?P<de>\d+)$',main_views.others_profile, name='others_profile'),
+  re_path(r'^follow/(?P<user_id>\d+)$',main_views.follow,name='follow'),
+  re_path(r'^unfollow/(?P<user_id>\d+)$',main_views.unfollow,name='unfollow'),
+  re_path(r'^delete/(?P<image_id>\d+)$',main_views.delete,name='delete'),
+  re_path(r'^deleteaccount/$',main_views.deleteaccount,name='deleteaccount'),
 
 ]
 
